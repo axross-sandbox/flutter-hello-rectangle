85cead0c64a36f8d1e59ffa991f85ba024910abf
axross <me@axross.io>
2018-12-11 21:52:54 -0800

    Implement the converter inside the converter route

--- lib/converter_route.dart
+++ lib/converter_route.dart
@@ -4,7 +4,7 @@ import './unit.dart';
 
 class ConverterRoute extends StatefulWidget {
   final String name;
-  final Color color;
+  final ColorSwatch color;
   final List<Unit> units;
 
   const ConverterRoute({
@@ -28,28 +28,154 @@ class _ConverterRouteState extends State<ConverterRoute> {
           centerTitle: true,
           backgroundColor: widget.color,
         ),
-        body: ListView(
-          children: widget.units
-              .map(
-                (unit) => Container(
-                      color: widget.color,
-                      margin: EdgeInsets.all(8),
-                      padding: EdgeInsets.all(8),
-                      child: Column(
-                        children: [
-                          Text(
-                            unit.name,
-                            style: Theme.of(context).textTheme.headline,
-                          ),
-                          Text(
-                            'Conversion: ${unit.conversion}',
-                            style: Theme.of(context).textTheme.subhead,
-                          ),
-                        ],
-                      ),
-                    ),
-              )
-              .toList(),
+        body: Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: <Widget>[
+              _ConverterGroup(
+                labelText: 'From',
+                units: widget.units,
+              ),
+              _CompareArrows(),
+              _ConverterGroup(
+                labelText: 'To',
+                units: widget.units,
+              ),
+            ],
+          ),
         ),
       );
+}
+
+class _ConverterGroup extends StatefulWidget {
+  final String labelText;
+  final List<Unit> units;
+
+  _ConverterGroup({
+    @required this.labelText,
+    @required this.units,
+  })  : assert(labelText != null),
+        assert(units != null),
+        assert(units.length >= 1);
+
+  @override
+  _ConverterGroupState createState() => _ConverterGroupState();
+}
+
+class _ConverterGroupState extends State<_ConverterGroup> {
+  final TextEditingController _textEditingController = TextEditingController();
+  bool _isInputtedValueValid = true;
+  Unit _selectedUnit;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _selectedUnit = widget.units.first;
+
+    _textEditingController.addListener(() {
+      setState(() {
+        final value = _textEditingController.text;
+
+        if (value.isEmpty) {
+          _isInputtedValueValid = true;
+
+          return;
+        }
+
+        try {
+          double.parse(value);
+        } on FormatException catch (error) {
+          _isInputtedValueValid = false;
+
+          return;
+        }
+
+        _isInputtedValueValid = true;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) => Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              controller: _textEditingController,
+              keyboardType: TextInputType.number,
+              style: Theme.of(context).textTheme.body1.copyWith(fontSize: 20),
+              decoration: InputDecoration(
+                labelText: widget.labelText,
+                filled: true,
+                fillColor: Colors.grey[200],
+                errorText: _isInputtedValueValid ? null : 'Invalid',
+                errorBorder: OutlineInputBorder(
+                  borderSide: BorderSide(),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 16),
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                    color: Colors.grey[600],
+                  ),
+                ),
+              ),
+              child: Container(
+                padding: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 7.0),
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(4),
+                    topRight: Radius.circular(4),
+                  ),
+                ),
+                child: DropdownButtonHideUnderline(
+                  child: DropdownButton(
+                    value: _selectedUnit,
+                    items: widget.units
+                        .map(
+                          (unit) => DropdownMenuItem(
+                                child: Text(unit.name),
+                                value: unit,
+                              ),
+                        )
+                        .toList(),
+                    onChanged: (Unit unit) {
+                      print(unit.name);
+
+                      setState(() {
+                        _selectedUnit = unit;
+                      });
+                    },
+                    style: Theme.of(context)
+                        .textTheme
+                        .body1
+                        .copyWith(fontSize: 20),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+}
+
+class _CompareArrows extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RotatedBox(
+      quarterTurns: 1,
+      child: Center(
+        child: Icon(
+          Icons.compare_arrows,
+          size: 40,
+        ),
+      ),
+    );
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,22 @@ import 'package:flutter/material.dart';
 import './category_route.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Unit Converter',
-      home: CategoryRoute(),
-    ),
-  );
+  runApp(UnitConverter());
+}
+
+class UnitConverter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Unit Converter',
+        theme: ThemeData(
+          textTheme: Theme.of(context).textTheme.apply(
+                bodyColor: Colors.black,
+                displayColor: Colors.grey[600],
+              ),
+          primaryColor: Colors.grey[500],
+          textSelectionHandleColor: Colors.green[500],
+        ),
+        home: CategoryRoute(),
+      );
 }
